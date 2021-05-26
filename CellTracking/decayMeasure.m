0e13a8e8b9b8ea1641f8eb1f5992d9f913f63c8d
za505 <za505@nyu.edu>
2021-05-26 15:20:56 -0400

    forgot to save 05182021 analysis, but it was incomplete and edits were minor

---
+++ CellTracking/decayMeasure.m
@@ -0,0 +1,94 @@
+%decayMeasure.m
+%Zarina Akbary, updated 05/25/21
+%Calculates decay as a function of frame rate and exposure
+
+clear, close all
+
+%INSTRUCTIONS FOR USE:
+%run photobleachMeasure.m first
+
+%INPUT
+%basename: experiments of interest
+%dirname: where .mat files are stored
+
+%OUTPUT:
+%timescale=array of time scales
+%Ts=array of T (number of frames)
+%switchF=array of switchFrames
+%A=array of A values (A*e^(-alpha*t)+y0)
+%f=cell of coeff for exponential eqxn
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%USER INPUT
+basenames=["05262021_FITCK_001", "05262021_FITCK_010"];
+dirname=['/Users/zarina/Downloads/NYU/Year2_2021_Spring/05262021_analysis/05262021_control/05262021_figures'];
+recrunch=0;
+vis=0;
+B=length(basenames);%number of main directories to analyze
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%go to directory where .mat files are stored
+cd(dirname)
+
+%pre-allocate variables
+timescales=nan(B,1);
+Ts=nan(B,1);
+time=cell(B,1);
+intensity=cell(B,1);
+coeff=nan(B,3);
+yhat=cell(B,1);
+A=nan(B,1);
+f=cell(B,1);
+yhat2=cell(B,1);
+
+%initialize coeff
+coeff0=[40000, -0.1, 0.1];
+
+%let's go through the mat files and store the variables we need
+for b=1:B
+    base=char(basenames(b))
+    load([base '_pb'])
+
+    timescales(b,1)=tscale;
+    Ts(b,1)=T;
+    times{b,1}=time;
+    intensity{b,1}=intensityAvg;
+    A(b,1)=intensityAvg(1,1);
+    
+    %here, we can fit to a nonlinear exponential eqxn
+    coeff(b,:)=nlinfit(times{b,1}, intensity{b,1}, @exponential, coeff0);
+    
+    %calculate the predicted y values
+    yhat{b,1}=exponential(coeff(b,:), times{b,1});
+    
+    %plot to see how well the eqxn fits the data
+    figure
+    scatter(times{b,1}, intensity{b,1}), hold on
+    plot(times{b,1}, yhat{b,1})
+    pause
+    saveas(gcf, [basename,'_exp1.fig'])
+    saveas(gcf, [basename,'_exp1.png'])
+    close
+    
+    %plot to see how well the eqxn fits the data
+    f{b,1}=fit(times{b,1}', intensity{b,1}', 'exp2');
+    figure
+    plot(f{b,1}, times{b,1}, intensity{b,1})
+    pause
+    saveas(gcf, [basename,'_exp2.fig'])
+    saveas(gcf, [basename,'_exp2.png'])
+    close
+    
+    %calculate new yhat values
+    yhat2{b,1}=f{b,1}(times{b,1});
+    
+    %find the half-life
+    idx=find(yhat2{b,1}==(yhat2{b,1}(1)/2))   
+end
+
+
+
+function [y] = exponential(b,x)
+%this function calculates y=A*(e^alpha*t)+y0
+%where a=A, b=alpha, c=t, and y0=y0;
+y=b(1)*exp(b(2)*x)+b(3);
+end
+
