a6d5295cf63e777210c00c5acfef4adace9a9609
za505 <za505@nyu.edu>
2021-06-20 09:21:47 -0400

    major troubleshooting and logic setting

--- CellTracking/PlasmolysisTrack.m
+++ CellTracking/PlasmolysisTrack.m
@@ -12,7 +12,7 @@ dirname=['/Users/zarina/Downloads/NYU/Year2_2021_Spring/05262021_analysis/' base
 %frame=4; %frame immediately upon hyperosmotic shock
 channels={'GFP', 'TADA'};
 vis=0; %to visualize the boundaries of the pre-shock cells plotted in post-shock frames
-preShock=1;
+preShock=2;
 postShock=3;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Load BackTrack.m data
@@ -29,11 +29,123 @@ ncellsT=ncells;
 
 %GFP data
 %cd([dirname '/' basename '_GFP/' basename '_figures']);
-load([basename '_BTgfp'], 'B', 'time', 'pixels', 'directory', 'T', 'ncells', 'acell', 'im');
+%load([basename '_BTgfp'], 'B', 'time', 'pixels', 'directory', 'T', 'ncells', 'acell', 'im');
+load([basename '_BTgfp'], 'directory');
 
 directory_GFP=directory;
-B_GFP=B;
-pixelsGFP=pixels;
+%B_GFP=B;
+%pixelsGFP=pixels;
+
+%% first, let's look at our TADA cells
+cd(directory_TADA(1).folder);
+im1=imread(directory_TADA(preShock).name);
+im2=imread(directory_TADA(postShock).name);
+
+imshowpair(im1, im2, 'montage')
+pause, close
+
+%what is the size difference b/t the pre and post-shock cells. This is the
+%difference that will be negated when looking at the ddifference between
+%pre- and post-shock GFP cells
+bw1=zeros(size(im));
+for i=1:ncellsT
+    if isempty(pixelsTADA{i,preShock})==0
+        bw1(pixelsTADA{i,preShock})=1;
+    end
+end
+
+bw2=zeros(size(im));
+for i=1:ncells
+    if isempty(pixelsTADA{i,postShock})==0
+        bw2(pixelsTADA{i,postShock})=1;
+    end
+end
+
+%potential prob: what if the # of cells tracked in the pre and post
+%shock TADA images don't match? have to add a line of code that takes this into
+%account
+
+%potential solution
+%create a matrix of all the pixels indicies of the tracked pre-shock TADA cells
+allpixels=[];
+allpixels2=[];
+for k=1:ncells
+    allpixels=[allpixels; pixels{k,preShock}];
+    allpixels2=[allpixels2; pixels{k,postShock}];
+end
+
+pdiff=setdiff(allpixels2,allpixels); %it's about 140 pixels
+pdiff2=setdiff(pdiff, allpixels); %these are all the shared pixels
+
+%this is the amount of area loss we expect from contraction
+bwe=bw1-bw2;
+imshow(bwe)
+pause, close
+
+%this is the area in the post-shock cells not subtracted by the pre-shock
+%frame. It is the same index as pdiff, and the values are all -1
+% bwc=bw2-bw1;
+% imshow(bwc)
+% pause,close
+% pidx=find(bwc==1);
+% pdiff2=setdiff(pidx, pdiff);
+% val=bwe(pdiff);
+
+%dilate the pre-shock cells and set the pixels in the post-shock cell not subtracted by the pre-shock cell
+%to 0 
+%nhood=structuring element neighborhood
+nhood=[0,1,0;1,1,1;0,1,0];
+bw1=imdilate(bw1, nhood);
+bwe=bw1-bw2;
+imshow(bwe)
+pause, close
+bwe(bwe==-1)=0;
+bwe(pdiff2)=1;
+bwe=bwmorph(bwe,'skel');
+imshow(bwe);
+
+%% now, let's look at the GFP cells
+cd(directory_GFP(1).folder);
+im3=imread(directory_GFP(preShock).name);
+im4=imread(directory_GFP(postShock).name);
+
+%trace the outline of the TADA tracked region onto the cell
+figure
+imshow(im3, [])
+hold on
+for k=1:ncells
+   if isempty(B{k,preShock})==0
+         plot(B_TADA{k,preShock}(:,1),B_TADA{k,preShock}(:,2),'-r')
+   end
+end   
+pause, close all
+
+figure
+imshow(im4, [])
+hold on
+for k=1:ncells
+   if isempty(B{k,preShock})==0
+         plot(B_TADA{k,preShock}(:,1),B_TADA{k,preShock}(:,2),'-r')
+   end
+end   
+pause, close all
+  
+%sanity check:
+%from the GFP image, set any pixels value not within the TADA region to 0
+didx=find(bwe==0);
+im3(didx)=0;
+im4(didx)=0;
+%okay, so the outline of our tada cell matches the outline of the GFP cell
+%so far. Do they cancel?
+im3(im3~=0)=1;
+im3=imbinarize(im3);
+bwf=im3-bwe;
+imshow(bwf)
+
+bw1=zeros(size(im));
+for k=1:ncells
+    bw1(im1(pixels{k,preShock})>1200)=1;
+end
 %% overlay TADA cell boundaries on GFP image stack
 cd(directory_GFP(1).folder);
 
@@ -47,14 +159,15 @@ for t=1:T
     im=imread(imagename);
     [imM,imN]=size(im);
     
-    cd(directory_TADA(1).folder);
-    im1=imread(directory_TADA(postShock).name);
+%     cd(directory_TADA(1).folder);
+%     im1=imread(directory_TADA(postShock).name);
 
-    cd(directory_GFP(1).folder);
-    im2=imread(directory_GFP(postShock).name);
+%     cd(directory_GFP(1).folder);
+%     im2=imread(directory_GFP(postShock).name);
 
     figure
-    imshowpair(im1, im2)
+    %imshowpair(im1, im2)
+    imshow(im, [])
     hold on
     for k=1:ncellsT
        if isempty(B_TADA{k,t})==0
@@ -63,11 +176,11 @@ for t=1:T
              plot(B_TADA{k,t}(:,1),B_TADA{k,t}(:,2),'-r')
        end
        
-       if isempty(B_GFP{k,t})==0
-%            [r1 c1]=ind2sub(size(im), pixels{k,t});
-%            plot(c1, r1)
-             plot(B_GFP{k,t}(:,1),B_GFP{k,t}(:,2),'--g')
-       end
+%        if isempty(B_GFP{k,t})==0
+% %            [r1 c1]=ind2sub(size(im), pixels{k,t});
+% %            plot(c1, r1)
+%              plot(B_GFP{k,t}(:,1),B_GFP{k,t}(:,2),'--g')
+%        end
     end
     
   pause
