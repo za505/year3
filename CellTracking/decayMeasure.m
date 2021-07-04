83e5a792c8e4def9ab489f911bc9ec6b913c16c5
za505 <za505@nyu.edu>
2021-07-04 15:31:31 -0400

    almost there, I think

--- CellTracking/decayMeasure.m
+++ CellTracking/decayMeasure.m
@@ -43,9 +43,12 @@ else
     %pre-allocate variables
     icell_green=[];
     icell_mcherry=[];
+    nGreen=[];
+    nCherry=[];
     
     %calculate time index
     tidx=1:2:T-1; %remember, fluor images were taken every 2 frames
+    tpt=480; %time at which *PBS+5% NLS is perfused
     
     for i=1:length(channels)
         
@@ -67,103 +70,24 @@ else
         for n=1:ncells
             if i==1 & mean(intensity(n, 1:5)>20000)
                 icell_green=[icell_green; intensity(n,:)];
+                nGreen=[nGreen n];
             elseif i==2 & mean(intensity(n, 1:5)>20000)
-                icell_cherry=[icell_cherry; intensity(n,:)];
+                icell_mcherry=[icell_mcherry; intensity(n,:)];
+                nCherry=[nCherry n];
             else
                 continue
             end
         end
     end
-end
-
-%% Plot data
-
-%define ncells
-ncells=height(boun);
-
-%figure out which traces belongs to which channel
-% figure, hold on
-% for n=1:height(icell{1:1})
-%     plot(time(tidx),icell{1:1}(n, tidx)) 
-% end
-
-nGreen=[1,3,6,7,15,17,18,22,23,24,25,26,32,33,35,38];
-nCherry=[2,4,5,8,9,10,11,12,14,16,19,20,21,29,31,36,39];
-%skipped 13,27,28,30,34, and 37 because over half the values are NaN
-
-%split the cell intensity array into separate variables based on channel
-icell_green=icell{1,1}(nGreen, tidx);
-icell_mcherry=icell{1,2}(nCherry, tidx);
-
-%identity time point where cells lyse
-tpt=480;
-
-%initialize coefficients
-coeff0=[40000, 0.1];
     
-for n=1:height(icell{1:1})
-    n
-    x=sum(isnan(icell{1,1}(n,tidx)))
+    cd(dirname)
+    save([basename '_dm.mat'])
 end
-
-%pre-allocate variables
-coeff1=nan(height(icell_green), length(coeff0));
-coeff2=nan(height(icell_mcherry), length(coeff0));
-yhat1=nan(height(icell_green), length(time));
-yhat2=nan(height(icell_mcherry), length(time));
-
-%here, we can fit each cell intensity to a nonlinear exponential eqxn
-for i=1:height(icell_green)
-    coeff1(i,:)=nlinfit(time, icell_green(i,:), @exponential, coeff0);
-    yhat1(i,:)=exponential(coeff1(i,:), time);
-end
-   
-for i=1:height(icell_mcherry)
-    coeff2(i,:)=nlinfit(time, icell_mcherry(i,:), @exponential, coeff0);
-    yhat2(i,:)=exponential(coeff2(i,:), time);
-end
-
-%plot to see how well the eqxn fits the data
-figure, hold on
-for i=1:height(icell_green)
-    scatter(time, icell_green(i,:))
-    %scatter(time, yhat1(i,:))
-end
-% saveas(gcf, [basename,'_expGreen.fig'])
-% saveas(gcf, [basename,'_expGreen.png'])
- 
-figure, hold on
-for i=1:height(icell_mcherry)
-    plot(time, icell_mcherry(i,:))
-    scatter(time, yhat2(i,:))
-end
-% saveas(gcf, [basename,'_expCherry.fig'])
-% saveas(gcf, [basename,'_expCherry.png'])
-
-%save(['dm'])
-
-end
-%%
-% coeff3=nan(height(icell_mcherry), length(coeff0));
-% yhat3=nan(height(icell_green), length(time));
-% for i=1:height(icell_green)
-%     coeff3(i,:)=nlinfit(time, icell_green(i,:), @linear, coeff0);
-%     yhat3(i,:)=linear(coeff3(i,:), time);
-% end
-% 
-% figure, hold on
-% for i=1:height(icell_green)
-%     plot(time, icell_green(i,:))
-%     scatter(time, yhat3(i,:))
-% end
-% saveas(gcf, [basename,'_expGreen2.fig'])
-% saveas(gcf, [basename,'_expGreen2.png'])
-
-%plot to see how well the eqxn fits the data
+%% Plot data
+%plot to see single traces of mNeonGreen cells
 figure(1), hold on
 for i=1:height(icell_green)
     plot(time(tidx), icell_green(i,:))
-    %scatter(time, yhat1(i,:))
 end
 xline(tpt, '--', {'Membrane Lysis'})
 title('Cellular Intensity of mNeonGreen vs Time')
@@ -172,6 +96,7 @@ ylabel('Cellular Intensity (A.U.)')
 saveas(gcf, [basename,'_fullGreen.fig'])
 saveas(gcf, [basename,'_fullGreen.png'])
  
+%plot to see single traces of mCherry cells
 figure(2), hold on
 for i=1:height(icell_mcherry)
    plot(time(tidx), icell_mcherry(i,:))
@@ -183,79 +108,100 @@ ylabel('Cellular Intensity (A.U.)')
 saveas(gcf, [basename,'_fullCherry.fig'])
 saveas(gcf, [basename,'_fullCherry.png'])
 
-intensity=cell(ncells,T-1);
+%% old code
+
+Z=cell(ncells,length(tidx));
+cg=0; %green count
+cc=0; %cherry count
 
 for n=1:ncells
     
     if ismember(n, nGreen)
-        [~, locB]=ismember(n, nGreen);
-        cd( strcat(dirname, '/', basenames(1), '/', basename, '_figures'))
+        cg=cg+1;
         
-        dx=max(boun{n,1}(:,1))-min(boun{n,1}(:,1)); %columns are the x direction
-        dy=max(boun{n,1}(:,2))-min(boun{n,1}(:,2)); %rows are the y direction
-        x=min(boun{n,1}(:,1))-dx/2:max(boun{n,1}(:,1))+dx/2;
-        y=min(boun{n,1}(:,2))-dy/2:max(boun{n,1}(:,2))+dy/2;
+        [~, locB]=ismember(n, nGreen);
+        cd(['/Users/zarina/Downloads/NYU/Year3_2021_Summer/06062021_analysis/' basename '_colony1/' basename '_mNeonGreen/'  basename '_figures'])
+        
+        dx=max(B{n,1}(:,1))-min(B{n,1}(:,1)); %columns are the x direction
+        dy=max(B{n,1}(:,2))-min(B{n,1}(:,2)); %rows are the y direction
+        x=min(B{n,1}(:,1))-dx/2:max(B{n,1}(:,1))+dx/2;
+        y=min(B{n,1}(:,2))-dy/2:max(B{n,1}(:,2))+dy/2;
         [X,Y] = meshgrid(x,y);
         
-        for t=1:2:T-1
-            t
-            intensity{n,t}=ones(size(boun{n,t},1),2);
-            intensity{n,t}=intensity{1,t}*65530;
+        for j=1:length(tidx)
+            t=tidx(j);
+            Z{n,j}=ones(size(B{n,t},1),2);
+            Z{n,j}=Z{1,t}*65530;
         end
 
-        v = VideoWriter('pt2_cyto','MPEG-4');
+        v = VideoWriter(strcat('mNeonGreen_', num2str(cg), '_intensity'),'MPEG-4');
         open(v);
 
         figure, hold on
-        for t=1:2:T
-    plot3(boun{1,t}(:,1),boun{1,t}(:,2),intensity2{1,t})
-    hold on
-    t=surf(X,Y,cyto{1,t})
-    rotate3d on;
-    t.EdgeColor = 'interp';
-    t.FaceColor = 'interp';
-    view(0,95)
-    frame = getframe(gcf);
-    writeVideo(v,frame);
-    pause
-    clf
-end
+        for j=1:length(tidx)
+            t=tidx(j);
+            plot3(B{n,t}(:,1),B{n,t}(:,2),Z{n,j})
+            hold on
+            t=surf(X,Y,cyto{1,t})
+            %rotate3d on;
+            t.EdgeColor = 'interp';
+            t.FaceColor = 'interp';
+            view(0,95)
+            frame = getframe(gcf);
+            writeVideo(v,frame);
+            pause(1)
+            clf
+        end
 
-close(v)
-close all
+        close(v)
+        close all
 
     elseif ismember(n, nCherry)
+        cc=cc+1;
+        
         [~, locB]=ismember(n, nCherry);
-        cd( strcat(dirname, '/', basenames(2), '/', basename, '_figures'))
+        cd(['/Users/zarina/Downloads/NYU/Year3_2021_Summer/06062021_analysis/' basename '_colony1/' basename '_mCherry/'  basename '_figures'])
+        
+        dx=max(B{n,1}(:,1))-min(B{n,1}(:,1)); %columns are the x direction
+        dy=max(B{n,1}(:,2))-min(B{n,1}(:,2)); %rows are the y direction
+        x=min(B{n,1}(:,1))-dx/2:max(B{n,1}(:,1))+dx/2;
+        y=min(B{n,1}(:,2))-dy/2:max(B{n,1}(:,2))+dy/2;
+        [X,Y] = meshgrid(x,y);
+        
+        for j=1:length(tidx)
+            t=tidx(j);
+            Z{n,j}=ones(size(B{n,t},1),2);
+            Z{n,j}=Z{1,t}*65530;
+        end
+
+        v = VideoWriter(strcat('mCherry_', num2str(cc), '_intensity'),'MPEG-4');
+        open(v);
+
+        figure, hold on
+        for j=1:length(tidx)
+            t=tidx(j);
+            plot3(B{n,t}(:,1),B{n,t}(:,2),Z{n,j})
+            hold on
+            t=surf(X,Y,cyto{1,t})
+            %rotate3d on;
+            t.EdgeColor = 'interp';
+            t.FaceColor = 'interp';
+            view(0,95)
+            frame = getframe(gcf);
+            writeVideo(v,frame);
+            pause(1)
+            clf
+        end
+
+        close(v)
+        close all
     else
         continue
     end
 end
         
 cd(savedir)
-% % v = VideoWriter('pt2_cyto','MPEG-4');
-% % open(v);
-% 
-
-% 
-% figure
-% hold on
-% for t=1:T
-%     plot3(boun{1,t}(:,1),boun{1,t}(:,2),intensity2{1,t})
-%     hold on
-%     t=surf(X,Y,cyto{1,t})
-%     rotate3d on;
-%     t.EdgeColor = 'interp';
-%     t.FaceColor = 'interp';
-%     %view(0,95)
-% %     frame = getframe(gcf);
-% %     writeVideo(v,frame);
-%     pause
-%     clf
-% end
-% 
-% %close(v)
-close all
+saveas([basename 'processed_dm.mat'])
 
 %%%%%%%%%%%Functions
 function [y] = exponential(b,x)
