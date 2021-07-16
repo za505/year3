ba2298607e2115495911a467eedb2550b353b029
za505 <za505@nyu.edu>
2021-07-16 07:29:04 -0400

    still trying to make it user interactive

--- CellTracking/decayMeasure.m
+++ CellTracking/decayMeasure.m
@@ -25,7 +25,7 @@ dirname=['/Users/zarina/Downloads/NYU/Year3_2021_Summer/07142021_analysis/' base
 savedir=['/Users/zarina/Downloads/NYU/Year3_2021_Summer/07142021_analysis/' basename '/' basename '_colony1/'  basename '_mNeonGreen/' basename '_figures'];%Directory to save the output .mat file to.
 channels={['/Users/zarina/Downloads/NYU/Year3_2021_Summer/07142021_analysis/' basename '/' basename '_colony1/' basename '_mNeonGreen/' basename '_aligned']}; 
 recrunch=0;
-replot=0;
+replot=1;
 troubleshoot=0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if recrunch==1
@@ -46,6 +46,7 @@ else
     icell_green=nan(ncells, T);
     norm_green=nan(ncells, T);
     f={};
+    model={};
     
     for i=1:length(channels)
         
@@ -63,34 +64,92 @@ else
             
         end
         
+        %normalize the intensity traces
         for n=1:ncells
             norm_green(n,:) = icell_green(n,:)./icell_green(n,1);
         end    
         
         for n=1:ncells
             
-            flag=0;
-            guess=1;
-            
-            while flag==0
-                
-                [f{n}, answer]=tryPlot(time, norm_green, n, guess);
-                guess=answer;
+            [xData, yData] = prepareCurveData(time, norm_green(n,:));
+            f{n}=fit(xData, yData, 'exp1');
+            model{n}=['exp1'];
+            plot(f{n}, time, norm_green(n,:))
+            pause
+
+            prompt = 'Is this a good fit? 1=Yes, 2=Try Two-Term Exponential ';
+            answer = input(prompt)
                 
                 if answer==1
                     cd(savedir)
+                    model{n}=['exp1'];
                     savePlot(time, norm_green, n, f{n}, basename);
-                    flag=1;
+                else
+                    [xData, yData] = prepareCurveData(time, norm_green(n,:));
+                    f{n}=fit(xData, yData, 'exp2');
+                    model{n}=['exp2'];
+                    plot(f{n}, time, norm_green(n,:))
+                    pause 
+                
+                    prompt = 'Is this a good fit? 1=first one was better, 2=this one is better ';
+                    answer2 = input(prompt)
+                        
+                        if answer2==1
+                            [xData, yData] = prepareCurveData(time, norm_green(n,:));
+                            f{n}=fit(xData, yData, 'exp1');
+                            model{n}=['exp1'];
+                            cd(savedir)
+                            savePlot(time, norm_green, n, f{n}, basename);
+                        else
+                            cd(savedir)
+                            savePlot(time, norm_green, n, f{n}, basename);
+                        end        
+     
                 end
-            end
-        end
+                
+                close
+         end
             
 
-    end
+end
     
 end
 
+%% Does only half the cell lose fluor?
+halfie=nan(ncells, 1);
 
+cd(channels{1}); 
+for k=1:ncells
+    
+    for t=T:T-5
+        t
+        imagename=fluo_directory{1}(t).name;
+        im=imread(imagename);
+
+
+       figure
+       imshow(im, [])
+       hold on
+       
+        if isempty(B{k,t})==0
+            plot(B{k,t}(:,1),B{k,t}(:,2),'-r')
+        else
+            continue
+        end
+        
+        pause
+        close all
+    end
+      
+     prompt = 'Is only half the cell fluor or dark? 0=No, 1=Yes ';
+     answer = input(prompt)
+     
+     halfie(k)=answer;
+     
+ end
+
+%% combine these variables into a table
+dataTable=table(lcell, icell_green, norm_green, f, model, halfie, 'VariableNames', {'cell length', 'intensity', 'normalized intensity', 'coefficients', 'model type', 'halfie'});
 %% Plot data
 if replot==1
     cd(savedir)
@@ -196,6 +255,7 @@ cd(savedir)
 save([basename '_dm.mat'])
     
 %% Functions
+
 function savePlot(time, norm_green, i, newModel, basename) 
                 figure('Name', num2str(i))
                 plot(newModel, time, norm_green(i,:))
@@ -206,99 +266,3 @@ function savePlot(time, norm_green, i, newModel, basename)
                 saveas(gcf, [basename '_' num2str(i) '_fitGreen.png'])
                 close  
 end
-
-function [newModel, answer]= tryPlot(time, norm_green, n, guess)
-            
-            if guess==1
-                
-                [xData, yData] = prepareCurveData(time, norm_green(n,:));
-                newModel=fit(xData, yData, 'exp1');
-                plot(newModel, time, norm_green(n,:))
-                pause
-                
-                prompt = 'Is this a good fit? 1=Yes, 2=Try Two-Term Exponential, 3=Try Linear, 4=Try Quadratic, 5=Try Piecewise Linear, 6=Piecewise Cubic ';
-                answer = input(prompt)
-                
-                close
-                
-            elseif guess==2
-                
-                [xData, yData] = prepareCurveData(time, norm_green(n,:));
-                newModel=fit(xData, yData, 'exp2');
-                plot(newModel, time, norm_green(n,:))
-                pause 
-                
-                prompt = 'Is this a good fit? 1=Yes, 2=Try Two-Term Exponential, 3=Try Linear, 4=Try Quadratic, 5=Try Piecewise Linear, 6=Piecewise Cubic ';
-                answer = input(prompt)
-                
-                close 
-                
-            elseif guess==3
-                
-                [xData, yData] = prepareCurveData(time, norm_green(n,:));
-                newModel=fit(xData, yData, 'poly1');
-                plot(newModel, time, norm_green(n,:))
-                pause
-                
-                prompt = 'Is this a good fit? 1=Yes, 2=Try Two-Term Exponential, 3=Try Linear, 4=Try Quadratic, 5=Try Piecewise Linear, 6=Piecewise Cubic ';
-                answer = input(prompt)
-                
-                close
-                
-            elseif guess==4
-                
-                [xData, yData] = prepareCurveData(time, norm_green(n,:));
-                newModel=fit(xData, yData, 'poly2');
-                plot(newModel, time, norm_green(n,:))
-                pause
-                
-                prompt = 'Is this a good fit? 1=Yes, 2=Try Two-Term Exponential, 3=Try Linear, 4=Try Quadratic, 5=Try Piecewise Linear, 6=Piecewise Cubic ';
-                answer = input(prompt)
-                
-                close
-                
-            elseif guess==5
-                
-                [xData, yData] = prepareCurveData(time, norm_green(n,:));
-                newModel=fit(xData, yData, 'linearinterp');
-                plot(newModel, time, norm_green(n,:))
-                pause
-                
-                prompt = 'Is this a good fit? 1=Yes, 2=Try Two-Term Exponential, 3=Try Linear, 4=Try Quadratic, 5=Try Piecewise Linear, 6=Piecewise Cubic ';
-                answer = input(prompt)
-
-                close
-                
-            elseif guess==6
-                
-                [xData, yData] = prepareCurveData(time, norm_green(n,:));
-                newModel=fit(xData, yData, 'cubicinterp');
-                plot(newModel, time, norm_green(n,:))
-                pause
-                
-                prompt = 'Is this a good fit? 1=Yes, 2=Try Two-Term Exponential, 3=Try Linear, 4=Try Quadratic, 5=Try Piecewise Linear, 6=Piecewise Cubic ';
-                answer = input(prompt)
-                
-                close
-                
-            else
-                
-                prompt = 'Are you sure? 0=No, 1=Yes '
-                response = input(prompt)
-                
-                if response==1
-                    [xData, yData] = prepareCurveData(time, norm_green(n,:));
-                    newModel=fit(xData, yData, 'exp1');
-                    plot(newModel, time, norm_green(n,:))
-                    pause(0.3)
-
-                    message='default: exponential'
-                    answer=1;
-                else
-                    answer=2;
-                end
-                
-            end
-            
-end
-        
