0c40f3906a772f64da0791adfa6514dc9dda7a1e
za505 <za505@nyu.edu>
2021-07-22 17:12:55 -0400

    latest update

--- CellTracking/diffusionAnalysis.m
+++ CellTracking/diffusionAnalysis.m
@@ -5,8 +5,8 @@
 
 clear, close all
 %% User Input
-dirname=['/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis'];
-photodir={['/Users/zarina/Downloads/NYU/Year3_2021_Summer/07172021_analysis/07172021_Exp1_colony1/07172021_Exp1_GFP/07172021_Exp1_figures'];['/Users/zarina/Downloads/NYU/Year3_2021_Summer/07172021_analysis/07172021_Exp1_colony2/07172021_Exp1_GFP/07172021_Exp1_figures']};
+maindir=['/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis'];
+photodir={['/Users/zarina/Downloads/NYU/Year3_2021_Summer/07102021_analysis/07102021_Exp1_colony1/07102021_Exp1_GFP/07102021_Exp1_figures'];['/Users/zarina/Downloads/NYU/Year3_2021_Summer/07102021_analysis/07102021_Exp1_colony2/07102021_Exp1_GFP/07102021_Exp1_figures']};
 recrunch=0;
 replot=1;
 %%%%%%%%%%%%%%%%%%%
@@ -14,7 +14,7 @@ replot=1;
 %% Part I: New Tables
 if recrunch==0
     % Load data
-    cd(dirname)
+    cd(maindir)
     dataDirectory=dir('*_dm.mat');
 
     fullData=table();
@@ -56,13 +56,46 @@ if recrunch==0
     end
 else
     % Load data
-    cd(dirname)
+    cd(maindir)
     load('diffusionAnalysis')
 end
 
 %% adjust the time variable
 time=time-time(1);
 
+%% load Cm control data
+for i=1:length(photodir)
+    cd(photodir{i});
+    photofiles=dir('*_dm.mat');
+    load(photofiles.name, 'dataTable');
+    phototime=cell2mat(struct2cell(load(photofiles.name, 'time')));
+    tempData=dataTable;
+    
+    %add label
+    experimentName=repelem(string(photofiles.name(1:end-7)), height(tempData))';
+    condition=repelem(string('CmControl'), height(tempData))';
+    
+    %create new table
+    newVar=table(experimentName, condition, 'VariableNames', {'Experiment', 'Condition'});
+    photoData=[newVar, tempData];
+end
+
+normMean=mean(photoData.intensity, 1, 'omitnan');
+normStd=std(photoData.intensity, 0, 1, 'omitnan');
+[xD, yD]=prepareCurveData(phototime, normMean);
+photofit=fit(xD, yD, 'exp1');
+photoextrap=photofit.a*exp(photofit.b*time);
+plot(photofit, xD, yD), hold on
+plot(time, photoextrap)
+title('Photobleaching Curve')
+legend({'data', 'fitted curve', 'extrapolation'})
+xlabel('Time (min)')
+ylabel('Intensity (A.U.)')
+cd(maindir)
+saveas(gcf, 'photobleachCurve.fig')
+saveas(gcf, 'photobleachCurve.png')
+
+dI=[0 abs(photoextrap(2:1:end)-photoextrap(1:1:end-1))];
 %% plot data
     
 %first, let's parse the data
@@ -72,7 +105,7 @@ cond3 = fullData(strcmp(fullData.Condition, "Mg2+")==1, :);
 
 if replot==0
     
-    cd(savedir);
+    cd(maindir);
     
 %% plot a histogram of the contraction lengths
 %is there a difference in lysis dL between halved and non-halved cells
@@ -260,20 +293,20 @@ yData3=cond3(cond3.halfie==0,:);
 
 %pre-allocate variables
 coeff1=nan(height(yData1),2);
-coeff2=nan(height(yData1),2);
-coeff3=nan(height(yData1),2);
+coeff2=nan(height(yData2),2);
+coeff3=nan(height(yData3),2);
 
 f1=cell(height(yData1),1);
-f2=cell(height(yData1),1);
-f3=cell(height(yData1),1);
+f2=cell(height(yData2),1);
+f3=cell(height(yData3),1);
 
 thalf1=nan(height(yData1),1);
-thalf2=nan(height(yData1),1);
-thalf3=nan(height(yData1),1);
+thalf2=nan(height(yData2),1);
+thalf3=nan(height(yData3),1);
 
 r1=nan(height(yData1),1);
-r2=nan(height(yData1),1);
-r3=nan(height(yData1),1);
+r2=nan(height(yData2),1);
+r3=nan(height(yData3),1);
 
 for n=1:height(yData1)
     
@@ -288,7 +321,7 @@ for n=1:height(yData1)
     
     if yData(1,end)<=cutoff
         csum=cumsum(yData<=cutoff);
-        idx=find(csum==1);
+        idx=min(find(csum==1));
         r1(n)=cutoff-yData(idx);
         thalf1(n)=time(idx);
     else
@@ -311,7 +344,7 @@ for n=1:height(yData2)
     
     if yData(1,end)<=cutoff
         csum=cumsum(yData<=cutoff);
-        idx=find(csum==1);
+        idx=min(find(csum==1));
         r2(n)=cutoff-yData(idx);
         thalf2(n)=time(idx);
     else
@@ -334,7 +367,7 @@ for n=1:height(yData3)
     
     if yData(1,end)<=cutoff
         csum=cumsum(yData<=cutoff);
-        idx=find(csum==1);
+        idx=min(find(csum==1));
         r3(n)=cutoff-yData(idx);
         thalf3(n)=time(idx);
     else
@@ -377,9 +410,173 @@ legend({'LB', 'EDTA', 'Mg^{2+}'})
 saveas(gcf, 'thalfHist.fig')
 saveas(gcf, 'thalfHist.png')
 
-cd(dirname)
+%% now adjust for photobleaching
+%is there a difference in normalized intensity?
+yData1=cond1(cond1.halfie==0,:);
+yData2=cond2(cond2.halfie==0,:);
+yData3=cond3(cond3.halfie==0,:);
+
+yData1=yData1.intensity + dI;
+yData2=yData2.intensity + dI;
+yData3=yData3.intensity + dI;
+
+figure(14)
+p1=plot(time, yData1, '-r'); hold on
+p2=plot(time, yData2, '-b'); 
+p3=plot(time, yData3, '-c'); 
+title('Intensity vs Time, photoadjusted')
+txt = ['{\color{red} red = LB, \color{blue} blue = EDTA, \color{cyan} cyan = Mg2+}'];
+subtitle(txt)
+xlabel('Time (min)')
+ylabel('Intensity (A.U.)')
+saveas(gcf, 'IntensityAdj.fig')
+saveas(gcf, 'IntensityAdj.png')
+
+yData1=yData1./yData1(:,1);
+yData2=yData2./yData2(:,1);
+yData3=yData3./yData3(:,1);
+
+figure(15)
+p1=plot(time, yData1, '-r'); hold on
+p2=plot(time, yData2, '-b'); 
+p3=plot(time, yData3, '-c'); 
+title('Normalized Intensity vs Time, photoadjusted')
+txt = ['{\color{red} red = LB, \color{blue} blue = EDTA, \color{cyan} cyan = Mg2+}'];
+subtitle(txt)
+xlabel('Time (min)')
+ylabel('Normalized Intensity (A.U.)')
+saveas(gcf, 'NormIntensityAdj.fig')
+saveas(gcf, 'NormIntensityAdj.png')
+
+%pre-allocate variables
+coeff1a=nan(height(yData1),2);
+coeff2a=nan(height(yData2),2);
+coeff3a=nan(height(yData3),2);
+
+f1a=cell(height(yData1),1);
+f2a=cell(height(yData2),1);
+f3a=cell(height(yData3),1);
+
+thalf1a=nan(height(yData1),1);
+thalf2a=nan(height(yData2),1);
+thalf3a=nan(height(yData3),1);
+
+r1a=nan(height(yData1),1);
+r2a=nan(height(yData2),1);
+r3a=nan(height(yData3),1);
+
+for n=1:height(yData1)
+    
+    yData=yData1(n,:);
+    [xData, yData]=prepareCurveData(time(1:length(yData)), yData);
+    f1a{n,1}=fit(xData, yData, 'exp1');
+    coeff1a(n,:)=coeffvalues(f1a{n});
+    
+    %yData=movingaverage(yData,5);
+    yData=yData';
+    cutoff=yData(1,1)/2;
+    
+    if yData(1,end)<=cutoff
+        csum=cumsum(yData<=cutoff);
+        idx=min(find(csum==1));
+        r1a(n)=cutoff-yData(idx);
+        thalf1a(n)=time(idx);
+    else
+        [xData, yData]=prepareCurveData(time(1:length(yData)), yData);
+        f=fit(xData, yData, 'poly1');
+        thalf1a(n)=(cutoff-f.p2)/f.p1;
+    end
+end
+
+for n=1:height(yData2)
+    n
+    yData=yData2(n,:);
+    [xData, yData]=prepareCurveData(time(1:length(yData)), yData);
+    f2a{n,1}=fit(xData, yData, 'exp1');
+    coeff2a(n,:)=coeffvalues(f2a{n});
+    
+    %yData=movingaverage(yData,5);
+    yData=yData';
+    cutoff=yData(1,1)/2;
+    
+    if yData(1,end)<=cutoff
+        csum=cumsum(yData<=cutoff);
+        idx=min(find(csum==1));
+        r2a(n)=cutoff-yData(idx);
+        thalf2a(n)=time(idx);
+    else
+        [xData, yData]=prepareCurveData(time(1:length(yData)), yData);
+        f=fit(xData, yData, 'poly1');
+        thalf2a(n)=(cutoff-f.p2)/f.p1;
+    end
+end
+
+for n=1:height(yData3)
+    
+    yData=yData3(n,:);
+    [xData, yData]=prepareCurveData(time(1:length(yData)), yData);
+    f3a{n,1}=fit(xData, yData, 'exp1');
+    coeff3a(n,:)=coeffvalues(f3a{n});
+    
+    %yData=movingaverage(yData,5);
+    yData=yData';
+    cutoff=yData(1,1)/2;
+    
+    if yData(1,end)<=cutoff
+        csum=cumsum(yData<=cutoff);
+        idx=min(find(csum==1));
+        r3a(n)=cutoff-yData(idx);
+        thalf3a(n)=time(idx);
+    else
+        [xData, yData]=prepareCurveData(time(1:length(yData)), yData);
+        f=fit(xData, yData, 'poly1');
+        thalf3a(n)=(cutoff-f.p2)/f.p1;
+    end
+end
+
+figure(16)
+h1=histogram(-1./coeff1a(:,2)); hold on
+h2=histogram(-1./coeff2a(:,2));
+h3=histogram(-1./coeff3a(:,2)); 
+%h1.Normalization = 'probability';
+%h1.BinWidth = 0.25;
+%h2.Normalization = 'probability';
+%h2.BinWidth = 0.25;
+h1.NumBins = 12;
+h2.NumBins = 12;
+h3.NumBins = 12;
+%h3.Normalization = 'probability';
+%h3.BinWidth = 0.25;
+title('Time Constant Distribution, Photobleach Adjusted')
+xlabel('Tau (-min)')
+legend({'LB', 'EDTA', 'Mg^{2+}'})
+saveas(gcf, 'tauHistAdj.fig')
+saveas(gcf, 'tauHistAdj.png')
+
+figure(17)
+h1=histogram(thalf1a); hold on
+h2=histogram(thalf2a);
+h3=histogram(thalf3a); 
+%h1.Normalization = 'probability';
+%h1.BinWidth = 0.25;
+%h2.Normalization = 'probability';
+%h2.BinWidth = 0.25;
+h1.NumBins = 12;
+h2.NumBins = 12;
+h3.NumBins = 12;
+%h3.Normalization = 'probability';
+%h3.BinWidth = 0.25;
+title('t_{1/2} Distribution, Photobleach Adjusted')
+xlabel('t_{1/2} (-min)')
+legend({'LB', 'EDTA', 'Mg^{2+}'})
+saveas(gcf, 'thalfHistAdj.fig')
+saveas(gcf, 'thalfHistAdj.png')
+
+%% save the data
+cd(maindir)
 save('diffusionAnalysis.mat')
 
+close all
 %% Functions
 % function [y]=sigmoidal(b,x)
 %     xData=x.*b(1);
