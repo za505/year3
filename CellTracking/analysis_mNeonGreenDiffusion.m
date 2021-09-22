55f3146d4190f0ad79726fd7dc4409fbc9f60834
za505 <za505@nyu.edu>
2021-09-22 06:06:43 -0400

    last attempt to compile all the different analyses together

--- CellTracking/analysis_mNeonGreenDiffusion.m
+++ CellTracking/analysis_mNeonGreenDiffusion.m
@@ -5,10 +5,10 @@ clear, close all
 
 %% user input
 datadir="/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis";
-dirsave="/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis/09142021_analysis";
+dirsave="/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis/09202021_analysis";
 presdir="/Users/zarina/Downloads/NYU/Year3_2021_Fall/updates/09142021";
 
-data = struct('experiment', [], 'colony', [], 'length', [], 'intensity', [], 'norm_intensity', [], 'time', [], 'a', [], 'b', [], 'c', [], 'd', [], 'fitModel', []);
+data = struct('experiment', [], 'colony', [], 'length', [], 'intensity', [], 'norm_intensity', [], 'time', [], 'fitModel', {});
 cd(datadir)
 
 files=dir('*.mat');
@@ -16,7 +16,6 @@ files=dir('*.mat');
 experiments=[];
 colonies=[];
 
-min=6000;
 for i=1:height(files)
     
     data(i).experiment=files(i).name(1:end-15);
@@ -31,10 +30,6 @@ for i=1:height(files)
     end
   
     data(i).time=time;
-    
-    if length(time) < min
-        min=length(time);
-    end
     
     line1=convertCharsToStrings(data(i).experiment);
     line2=convertCharsToStrings(data(i).colony);
@@ -57,6 +52,7 @@ end
 %PBS, frame rate = 1, incubation = 2 min, time = [0:90]
 %PBS, frame rate = 1, incubation = 8 min, time = [0:1:45]
 %PBS, frame rate = 1, incubation = 16 min, time = [0:1:45]
+%PBS, frame rate = 1, incubation = 20 min, time = [0:1:45]
 
 %annotation=["Mg2+", "LB, fr=2", "EDTA", "sodium azide", "sodium azide", "LB, fr=1", "PBS, 2 min", "PBS, 8 min", "PBS, 16 min"];
 SA1=116:123;
@@ -65,372 +61,263 @@ PBS1=239:340;
 PBS2=341:417;
 PBS3=418:462;
 %% fit data to exp2
-for i=1:length(data)
-    [data(i).a, data(i).b, data(i).c, data(i).d, data(i).fitModel] = expFitting(data(i).norm_intensity, data(i).time);
+for i=26:46 %1:length(data)
+    for j=1:height(data(i).norm_intensity)
+        j
+        if isnan(data(i).norm_intensity(j,:))==0
+            [xData, yData]=prepareCurveData(data(i).time/60, data(i).norm_intensity(j,:));
+            f = fit(xData, yData, '(1-B)*exp(-x/tau)+B', 'Lower', [0, 0], 'Upper', [2, Inf], 'StartPoint', [0.3,0.8]);
+            
+            data(i).fitModel{j,1}=f;
+            
+%             figure
+%             plot(data(i).fitModel{j,1}, data(i).time/60, data(i).norm_intensity(j,:))
+%             title([data(i).experiment ',' data(i).colony])
+%             xlabel('Time (hours)')
+%             ylabel('Normalized Intensity (A.U.)')
+%             pause, close
+        else
+            [data(i).fitModel{j,1}] = [];
+        end
+    end
 end
 
-%% Convert to a table
-% nrow=length(colonies);
-% times=nan(nrow, min);
-% lengths=nan(nrow, min);
-% intensities=nan(nrow, min);
-% norm_intensities=nan(nrow, min);
-% A=nan(nrow, 1);
-% B=nan(nrow, 1);
-% C=nan(nrow, 1);
-% D=nan(nrow, 1);
+%% plot tau for each distribution, then plot tau over w (where w is incubation time in PBS)
+pbs1=26:31;
+pbs2=32:37;
+pbs3=38:42;
+pbs4=43:46;
 
-times=[];
-lengths=[];
-intensities=[];
-norm_intensities=[];
-A=[];
-B=[];
-C=[];
-D=[];
+cd(dirsave)
 
-for i=1:length(data)
-    i
-    
-    time=repmat(data(i).time(1:min), height(data(i).length),1);
-    times=[times; time];
-    
-    lengths=[lengths; data(i).length(:,1:min)];
-    intensities=[intensities; data(i).intensity(:, 1:min)];
-    norm_intensities=[norm_intensities; data(i).norm_intensity(:, 1:min)];
-    A=[A; data(i).a(:, 1)];
-    B=[B; data(i).b(:, 1)];
-    C=[C; data(i).c(:, 1)];
-    D=[D; data(i).d(:, 1)];   
+figure, hold on  
+for i=pbs1
+    for j=1:height(data(i).norm_intensity)
+        if isnan(data(i).norm_intensity(j,:))==0
+           plot(data(i).time, data(i).norm_intensity(j,:), 'r')
+        end
+    end
 end
+xlabel('Time (minutes)')
+ylabel('Normalized Fluorescence (A.U.)')
+title('Time vs Normalized Fluorescence, 2 min incubation')
+saveas(gcf, 'pbs_2min.png')
+saveas(gcf, 'pbs_2min.fig')
 
-dataTable=table(categorical(experiments), categorical(colonies), times, lengths, intensities, norm_intensities, A, B, C, D, 'VariableNames', {'experiment', 'colony', 'time', 'length', 'intensity', 'norm_intensity','a', 'b', 'c', 'd'});
-%% plot sodium azide data
-cd(presdir)
+figure, hold on  
+for i=pbs2
+    for j=1:height(data(i).norm_intensity)
+        if isnan(data(i).norm_intensity(j,:))==0
+           plot(data(i).time, data(i).norm_intensity(j,:), 'b')
+        end
+    end
+end
+xlabel('Time (minutes)')
+ylabel('Normalized Fluorescence (A.U.)')
+title('Time vs Normalized Fluorescence, 8 min incubation')
+saveas(gcf, 'pbs_8min.png')
+saveas(gcf, 'pbs_8min.fig')
 
+figure, hold on  
+for i=pbs3
+    for j=1:height(data(i).norm_intensity)
+        if isnan(data(i).norm_intensity(j,:))==0
+           plot(data(i).time, data(i).norm_intensity(j,:), 'g')
+        end
+    end
+end
+xlabel('Time (minutes)')
+ylabel('Normalized Fluorescence (A.U.)')
+title('Time vs Normalized Fluorescence, 16 min incubation')
+saveas(gcf, 'pbs_16min.png')
+saveas(gcf, 'pbs_16min.fig')
+
+figure, hold on  
+for i=pbs4
+    for j=1:height(data(i).norm_intensity)
+        if isnan(data(i).norm_intensity(j,:))==0
+           plot(data(i).time, data(i).norm_intensity(j,:), 'k')
+        end
+    end
+end
+xlabel('Time (minutes)')
+ylabel('Normalized Fluorescence (A.U.)')
+title('Time vs Normalized Fluorescence, 20 min incubation')
+saveas(gcf, 'pbs_20min.png')
+saveas(gcf, 'pbs_20min.fig')
+
+[b1, tau1] = coeffExt(data, pbs1)
 figure(1), hold on
-for i=SA1
-    plot(times(i,:), lengths(i,:), '-r')
-end
-for i=SA2
-    plot(times(i,:), lengths(i,:), '-b')
-end
-xlabel('Time (minutes)')
-ylabel('Length (\mum)')
-title('Length vs Time, Sodium Azide (07242021 in red, 08172021 in blue)')
-saveas(gcf, 'length_SA.png')
-saveas(gcf, 'length_SA.fig')
+h1=histogram(tau1)
+h1.FaceColor = 'r';
+h1.EdgeColor = 'k';
+h1.BinWidth = 0.1;
+xlabel('tau')
+ylabel('Counts')
+title('Distribution of tau Coefficient for 2 min incubation')
+saveas(gcf, 'tau1_2min.png')
+saveas(gcf, 'tau1_2min.fig')
 
+[b2, tau2] = coeffExt(data, pbs2)
 figure(2), hold on
-for i=SA1
-    plot(times(i,:), norm_intensities(i,:), '-r')
-end
-for i=SA2
-    plot(times(i,:), norm_intensities(i,:), '-b')
-end
-xlabel('Time (minutes)')
-ylabel('Normalized Intensity (A.U.)')
-title('Normalized Intensity vs Time, Sodium Azide (07242021 in red, 08172021 in blue)')
-saveas(gcf, 'normintensity_SA.png')
-saveas(gcf, 'normintensity_SA.fig')
+h2=histogram(tau2)
+h2.FaceColor = 'b';
+h2.EdgeColor = 'k';
+h2.BinWidth = 0.1;
+xlabel('tau')
+ylabel('Counts')
+title('Distribution of tau Coefficient for 8 min incubation')
+saveas(gcf, 'tau1_8min.png')
+saveas(gcf, 'tau1_8min.fig')
 
+[b3, tau3] = coeffExt(data, pbs3)
 figure(3), hold on
-for i=SA1
-    plot(times(i,:), intensities(i,:), '-r')
-end
-for i=SA2
-    plot(times(i,:), intensities(i,:), '-b')
-end
-xlabel('Time (minutes)')
-ylabel('Intensity (A.U.)')
-title('Intensity vs Time, Sodium Azide (07242021 in red, 08172021 in blue)')
-saveas(gcf, 'intensity_SA.png')
-saveas(gcf, 'intensity_SA.fig')
+h3=histogram(tau3)
+h3.FaceColor = 'c';
+h3.EdgeColor = 'k';
+h3.BinWidth = 0.1;
+xlabel('tau')
+ylabel('Counts')
+title('Distribution of tau Coefficient for 16 min incubation')
+saveas(gcf, 'tau1_16min.png')
+saveas(gcf, 'tau1_16min.fig')
 
-close all
-%% plot PBS data
-cd(presdir)
-
-figure(1), hold on
-for i=PBS1
-    plot(times(i,:), lengths(i,:), '-r')
-end
-for i=PBS2
-    plot(times(i,:), lengths(i,:), '-b')
-end
-for i=PBS3
-    plot(times(i,:), lengths(i,:), '-c')
-end
-xlabel('Time (minutes)')
-ylabel('Length (\mum)')
-title('Length vs Time, 2 min=red, 8 min=blue, 16 min=cyan')
-saveas(gcf, 'length_PBS.png')
-saveas(gcf, 'length_PBS.fig')
-
-figure(2), hold on
-for i=PBS1
-    plot(times(i,:), norm_intensities(i,:), '-r')
-end
-for i=PBS2
-    plot(times(i,:), norm_intensities(i,:), '-b')
-end
-for i=PBS3
-    plot(times(i,:), norm_intensities(i,:), '-c')
-end
-xlabel('Time (minutes)')
-ylabel('Normalized Intensity (A.U.)')
-title('Normalized Intensity vs Time, 2 min=red, 8 min=blue, 16 min=cyan')
-saveas(gcf, 'normintensity_PBS.png')
-saveas(gcf, 'normintensity_PBS.fig')
-
-figure(3), hold on
-for i=PBS1
-    plot(times(i,:), intensities(i,:), '-r')
-end
-for i=PBS2
-    plot(times(i,:), intensities(i,:), '-b')
-end
-for i=PBS3
-    plot(times(i,:), intensities(i,:), '-c')
-end
-xlabel('Time (minutes)')
-ylabel('Intensity (A.U.)')
-title('Intensity vs Time, 2 min=red, 8 min=blue, 16 min=cyan')
-saveas(gcf, 'intensity_PBS.png')
-saveas(gcf, 'intensity_PBS.fig')
-
-%% Histogram plots of PBS alpha
-flagged=find(A>1.5|A<0);
+[b4, tau4] = coeffExt(data, pbs4)
 figure(4), hold on
-for i=PBS1
-    if ismember(i, flagged)==0
-        h1=histogram(A(i,:))
-        h1.FaceColor = 'r';
-        h1.EdgeColor = 'k';
-        h1.NumBins = 1;
-        h1.BinWidth = 0.1;
-    end
-end
-xlabel('alpha')
+h4=histogram(tau4)
+h4.FaceColor = 'm';
+h4.EdgeColor = 'k';
+h4.BinWidth = 0.1;
+xlabel('tau')
 ylabel('Counts')
-title('Distribution of alpha coefficients, 2 min=red, 8 min=blue, 16 min=cyan')
-saveas(gcf, 'hist_A_PBS1.png')
-saveas(gcf, 'hist_A_PBS1.fig')
+title('Distribution of tau Coefficient for 20 min incubation')
+saveas(gcf, 'tau1_20min.png')
+saveas(gcf, 'tau1_20min.fig')
 
-figure(5), hold on
-for i=PBS2
-    if ismember(i, flagged)==0
-        h2=histogram(A(i,:))
-        h2.FaceColor = 'b';
-        h2.EdgeColor = 'k';
-        h2.NumBins = 1;
-        h2.BinWidth = 0.1;
-    end
-end
-xlabel('alpha')
-ylabel('Counts')
-title('Distribution of alpha coefficients, 2 min=red, 8 min=blue, 16 min=cyan')
-saveas(gcf, 'hist_A_PBS2.png')
-saveas(gcf, 'hist_A_PBS2.fig')
+tau=[tau1; tau2; tau3; tau4];
+w=[repelem(2/60, length(tau1))'; repelem(8/60, length(tau2))'; repelem(16/60, length(tau3))'; repelem(20/60, length(tau4))'];
+
+figure(5)
+scatter(w, tau)
+xlabel('omega (incubation time in hours)')
+ylabel('tau (h^-1)')
+saveas(gcf, 'tau_omega.png')
+saveas(gcf, 'tau_omega.fig')
+
+tau_avg=[mean(tau1); mean(tau2); mean(tau3); mean(tau4)];
+tau_std=[std(tau1); std(tau2); std(tau3); std(tau4)];
+w_avg=[2; 8; 16; 20];
 
 figure(6), hold on
-for i=PBS3
-    if ismember(i, flagged)==0
-        h3=histogram(A(i,:))
-        h3.FaceColor = 'c';
-        h3.EdgeColor = 'k';
-        h3.NumBins = 1;
-        h3.BinWidth = 0.1;
-    end
-end
-xlabel('alpha')
-ylabel('Counts')
-title('Distribution of alpha coefficients, 2 min=red, 8 min=blue, 16 min=cyan')
-saveas(gcf, 'hist_A_PBS3.png')
-saveas(gcf, 'hist_A_PBS3.fig')
+% errorbar(w_avg, tau_avg,-tau_std,tau_std,'o')
+errorbar(2, mean(tau1),-std(tau1), std(tau1),'o')
+errorbar(8, mean(tau2),-std(tau2), std(tau2),'o')
+errorbar(16, mean(tau3),-std(tau3), std(tau3),'o')
+errorbar(20, mean(tau4),-std(tau4), std(tau4),'o')
+legend({'2 min', '8 min', '16 min', '20 min'})
+xlabel('omega (incubation time in minutes)')
+ylabel('average tau (h^-1)')
+xlim([0, 22])
+saveas(gcf, 'tau_omega_avg.png')
+saveas(gcf, 'tau_omega_avg.fig')
 
-close all
-%% Histogram plots of PBS beta
-flagged=find(A>1.5|A<0);
-figure(4), hold on
-for i=PBS1
-    if ismember(i, flagged)==0
-        h1=histogram(B(i,:))
-        h1.FaceColor = 'r';
-        h1.EdgeColor = 'k';
-        h1.NumBins = 1;
-        h1.BinWidth = 0.1;
-    end
-end
-
-for i=PBS2
-    if ismember(i, flagged)==0
-        h2=histogram(B(i,:))
-        h2.FaceColor = 'b';
-        h2.EdgeColor = 'k';
-        h2.NumBins = 1;
-        h2.BinWidth = 0.1;
-    end
-end
-
-for i=PBS3
-    if ismember(i, flagged)==0
-        h3=histogram(B(i,:))
-        h3.FaceColor = 'c';
-        h3.EdgeColor = 'k';
-        h3.NumBins = 1;
-        h3.BinWidth = 0.1;
-    end
-end
-xlabel('beta')
-ylabel('Counts')
-title('Distribution of beta coefficients, 2 min=red, 8 min=blue, 16 min=cyan')
-saveas(gcf, 'hist_B_PBS.png')
-saveas(gcf, 'hist_B_PBS.fig')
-
-close all
-
-%% Histogram plots of PBS C
-flagged=find(A>1.5|A<0);
-figure(4), hold on
-for i=PBS1
-    if ismember(i, flagged)==0
-        h1=histogram(C(i,:))
-        h1.FaceColor = 'r';
-        h1.EdgeColor = 'k';
-        h1.NumBins = 1;
-        h1.BinWidth = 0.1;
-    end
-end
-xlabel('C')
-ylabel('Counts')
-title('Distribution of C coefficients, 2 min=red')
-saveas(gcf, 'hist_C_PBS1.png')
-saveas(gcf, 'hist_C_PBS1.fig')
-
-figure(5), hold on
-for i=PBS2
-    if ismember(i, flagged)==0
-        h2=histogram(C(i,:))
-        h2.FaceColor = 'b';
-        h2.EdgeColor = 'k';
-        h2.NumBins = 1;
-        h2.BinWidth = 0.1;
-    end
-end
-xlabel('C')
-ylabel('Counts')
-title('Distribution of C coefficients,8 min=blue')
-saveas(gcf, 'hist_C_PBS2.png')
-saveas(gcf, 'hist_C_PBS2.fig')
-
-figure(6), hold on
-for i=PBS3
-    if ismember(i, flagged)==0
-        h3=histogram(C(i,:))
-        h3.FaceColor = 'c';
-        h3.EdgeColor = 'k';
-        h3.NumBins = 1;
-        h3.BinWidth = 0.1;
-    end
-end
-xlabel('C')
-ylabel('Counts')
-title('Distribution of C coefficients, 16 min=cyan')
-saveas(gcf, 'hist_C_PBS3.png')
-saveas(gcf, 'hist_C_PBS3.fig')
-
-close all
-
-%% Histogram plots of PBS delta
-flagged=find(A>1.5|A<0);
-figure(4), hold on
-for i=PBS1
-    if ismember(i, flagged)==0
-        h1=histogram(D(i,:))
-        h1.FaceColor = 'r';
-        h1.EdgeColor = 'k';
-        h1.NumBins = 1;
-        h1.BinWidth = 0.1;
-    end
-end
-title('Distribution of delta coefficients, 2 min=red')
-saveas(gcf, 'hist_D_PBS1.png')
-saveas(gcf, 'hist_D_PBS1.fig')
-
-figure(5), hold on
-for i=PBS2
-    if ismember(i, flagged)==0
-        h2=histogram(D(i,:))
-        h2.FaceColor = 'b';
-        h2.EdgeColor = 'k';
-        h2.NumBins = 1;
-        h2.BinWidth = 0.1;
-    end
-end
-xlabel('delta')
-ylabel('Counts')
-title('Distribution of delta coefficients, 8 min=blue')
-saveas(gcf, 'hist_D_PBS2.png')
-saveas(gcf, 'hist_D_PBS2.fig')
-
-figure(6), hold on
-for i=PBS3
-    if ismember(i, flagged)==0
-        h3=histogram(D(i,:))
-        h3.FaceColor = 'c';
-        h3.EdgeColor = 'k';
-        h3.NumBins = 1;
-        h3.BinWidth = 0.1;
-    end
-end
-xlabel('delta')
-ylabel('Counts')
-title('Distribution of delta coefficients, 16 min=cyan')
-saveas(gcf, 'hist_D_PBS3.png')
-saveas(gcf, 'hist_D_PBS3.fig')
-
-%close all
 %% save data
 cd(dirsave)
 save('fitting.mat')
-%writetable(dataTable, 'dataTable.csv')
 
+%% Sodium Azide Experiment
+clear, close all
+dirsave="/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis/09202021_analysis";
+cd('/Users/zarina/Downloads/NYU/Year3_2021_Fall/09182021_analysis/09182021_Exp1/09182021_Exp1_colony1/09182021_Exp1_mNeonGreen/09182021_Exp1_figures')
+load('09182021_Exp1_dm.mat')
+
+cd(dirsave)
+figure(7), hold on
+for i=1:height(norm_intensity)
+    plot(time, norm_intensity(i,:), '-b')
+end
+xlabel('Time (minutes)')
+ylabel('Intensity (A.U.)')
+saveas(gcf, 'azide_normintensity.png')
+saveas(gcf, 'azide_normintensity.fig')
+
+figure(8), hold on
+for i=1:height(icell_intensity)
+    plot(time, icell_intensity(i,:), '-b')
+end
+xlabel('Time (minutes)')
+ylabel('Normalized Intensity (A.U.)')
+saveas(gcf, 'azide_intensity.png')
+saveas(gcf, 'azide_intensity.fig')
+ 
+dm=nan(ncells, T-1);
+dx = diff(norm_intensity, 1, 2);
+dy = diff(time/60);
+for i=1:height(dx)
+    dm(i, :) = dy./dx(i,:);
+    idx = find(abs(dm(i,:))>10);
+    dm(i, idx)=NaN;
+end
+
+dm_avg = mean(dm, 1, 'omitnan');
+p1 = [mean(dm_avg(:, 4:6), 2, 'omitnan'), std(dm_avg(:, 4:6), 0, 2, 'omitnan')];
+p2 = [mean(dm_avg(:, 7:11), 2, 'omitnan'), std(dm_avg(:, 7:11), 0, 2, 'omitnan')];
+p3 = [mean(dm_avg(:, 12:22), 2, 'omitnan'), std(dm_avg(:, 12:22), 0, 2, 'omitnan')];
+p4 = [mean(dm_avg(:, 23:41), 2, 'omitnan'), std(dm_avg(:, 23:41), 0, 2, 'omitnan')];
+
+dm_comp = [p1;p2;p3;p4];
+
+figure(9), hold on
+for i=1:ncells
+   scatter(time(1,1:end-1)/60, dm(i,:), 'b')
+end
+
+figure(10)
+%scatter(time(1,1:end-1)/60, mean(dm, 1))
+errorbar(time(1,1:end-1)/60, mean(dm, 1),-std(dm, 0,1),std(dm, 0,1),'o')
+ylabel('Slope, Normalized Intensity (A.U.)/Time (h)')
+xlabel('Time (h)')
+
+%fr = [repelem(120, 6), repelem(60, 6), repelem(30, 11), repelem(15,21), repelem(7.5,41)];
+fr = [120, 60, 30, 15, 7.5];
+figure(11), hold on
+%scatter(time(1,1:end-1)/60, mean(dm, 1))
+errorbar(fr(1), p1(1), -p1(2), p1(2),'o')
+errorbar(fr(2), p2(1), -p2(2), p2(2),'o')
+errorbar(fr(3), p3(1), -p3(2), p3(2),'o')
+errorbar(fr(4), p4(1), -p4(2), p4(2),'o')
+ylabel('Slope, Normalized Intensity (A.U.)/Time (h)')
+xlabel('Frame Rate (s)')
+xlim([0, 122])
+legend({'120 s', '60 s', '30 s', '15 s', '7.5 s'})
+title('Slope as a Function of Frame Rate')
+saveas(gcf, 'azide_fr.png')
+saveas(gcf, 'azide_fr.fig')
 %% functions
-function [a, b, c, d, fitModel] = expFitting(norm_green, time)
-
-    fitModel=cell(height(norm_green), 1);
-    a=nan(height(norm_green), 1);
-    b=nan(height(norm_green), 1);
-    c=nan(height(norm_green), 1);
-    d=nan(height(norm_green), 1);
-    %fitScore=nan(height(norm_green), 1);
-    
-    for n=1:height(norm_green)
-        if isnan(norm_green(n, 1))==0
-            [xData, yData]=prepareCurveData(time, norm_green(n,:));
-            %figure
-            fitModel{n,1}=fit(xData, yData, 'exp2'); %fit to a linear exponential
-            
-            %if isnan(fitModel{n,1})==0    
-                a(n,1) = fitModel{n,1}.a;
-                b(n,1) = fitModel{n,1}.b;
-                c(n,1) = fitModel{n,1}.c;
-                d(n,1) = fitModel{n,1}.d;
-    %             plot(f{n,1}, xData, yData)
-    %             prompt = 'Q1: Is this a good fit? 0=No, 1=Yes ';
-    %             fitScore(n,1) = input(prompt);
-    %             close
-            %else
-             %   fitModel{n,1}=NaN;
-            %end
-        else
-            fitModel{n,1}=NaN;
-            %fitScore(n,1)=NaN;
-            a(n,1)=NaN;
-            b(n,1)=NaN;
-            c(n,1)=NaN;
-            d(n,1)=NaN;    
+function [b, tau]=coeffExt(data, range)
+    b=[];
+    tau=[];
+    for i=range
+        for j=1:height(data(i).fitModel)
+            if isempty(data(i).fitModel{j,1})==0
+                coeffs = coeffvalues(data(i).fitModel{j,1})
+                b=[b; coeffs(1)];
+                tau=[tau; coeffs(2)];
+            end
         end
     end
+end
+
+function [th] = halfLife(data, range)
+    figure, hold on
+    
+    for i=range
+        for j=1:height(data(i).norm_intensity)
+            if isnan(data(i).norm_intensity(j,:))==0
+               plot(data(i).time, data(i).norm_intensity(j,:))
+            end
+        end
+    end
+    
+    pause, close
 end
