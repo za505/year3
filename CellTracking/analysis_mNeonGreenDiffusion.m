4be1c9b00b49946a7d98b7cacc30d7cba24246ed
za505 <za505@nyu.edu>
2021-09-30 11:03:02 -0400

    fitting to stepwise photobleaching control with fitnlm

--- CellTracking/analysis_mNeonGreenDiffusion.m
+++ CellTracking/analysis_mNeonGreenDiffusion.m
@@ -5,61 +5,79 @@
 
 clear, close all
 
-%% user input and load data
+%% load sodium azide
 datadir="/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis";
-dirsave="/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis/09222021_analysis";
-basename='09182021_Exp1_colony';
-eidx=79;
+dirsave="/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis/09272021_analysis";
+basename='09222021_Exp1_colony';
 
-data = struct('experiment', [], 'colony', [], 'length', [], 'intensity', [], 'norm_intensity', [], 'time', [], 'fitModel', {});
+%store directory
+%data = struct('experiment', [], 'colony', [], 'length', [], 'intensity', [], 'norm_intensity', [], 'time', [], 'fitModel', {});
 cd(datadir)
+intensity_files=dir([basename '*dm.mat']);
 
-files=dir([basename '*.mat']);
+%pre-allocate matrices
+data_intensity=[];
+data_norm_intensity=[];
 
-for i=1:height(files)
+%populate matrices
+for i=1:height(intensity_files)
     
-    data(i).experiment=files(i).name(1:end-15);
-    data(i).colony=files(i).name(15:21);
-    load(files(i).name, 'dataTable', 'time')
-    data(i).length=table2array(dataTable(dataTable.halfie==0, 1));
-    data(i).intensity=table2array(dataTable(dataTable.halfie==0, 2));
-    data(i).norm_intensity=table2array(dataTable(dataTable.halfie==0, 3));
+    load(intensity_files(i).name, 'dataTable', 'time', 'tidx')
     
-    data(i).length=data(i).length(:, 1:eidx);
-    data(i).intensity=data(i).intensity(:, 1:eidx);
-    data(i).norm_intensity=data(i).norm_intensity(:, 1:eidx);
-    
-    if time(1)~=0 
-        time = time-time(1);
-    end
-  
-    data(i).time=time(1:eidx)
-    
+    data_intensity=[data_intensity; table2array(dataTable(dataTable.halfie==0, 2))];
+    data_norm_intensity=[data_norm_intensity; table2array(dataTable(dataTable.halfie==0, 3))];
+
+    intensity_time=time;
 end
 
+%store directory
+length_files=dir([basename '*BTphase.mat']);
+
+%pre-allocate matrices
+data_length=[];
+
+%populate matrices
+for i=1:height(length_files)
+    
+    load(length_files(i).name, 'lcell', 'time')
+    
+    data_length=[data_length; lcell];
+
+    length_time=time;
+end
 %% plot length and intensity
 cd(dirsave)
 
 figure(1), hold on
-for i=1:height(data)
-    for j=1:height(data(i).length)
-        plot(data(i).time, data(i).length(j,:), '-r')
-    end
+for i=1:height(data_length)
+    plot(length_time/60, data_length(i,:), '-g')
 end
 
 figure(2), hold on
-for i=1:height(data)
-    for j=1:height(data(i).intensity)
-        plot(data(i).time, data(i).intensity(j,:), '-r')
-    end
+for i=1:height(data_intensity)
+    plot(intensity_time, data_intensity(i,:), '-g')
 end
 
 figure(3), hold on
-for i=1:height(data)
-    for j=1:height(data(i).norm_intensity)
-        plot(data(i).time, data(i).norm_intensity(j,:), '-r')
-    end
+for i=1:height(data_norm_intensity)
+    plot(intensity_time, data_norm_intensity(i,:), '-g')
 end
 
 %% fit data to exponential function
+segment1=data_norm_intensity(:, 1:11);
+segment2=data_norm_intensity(:, 12:32);
+segment3=data_norm_intensity(:, 33:end);
 
+beta=0.99;
+modelfun=@(coeff,x)(1-beta)*exp(-x./coeff)+beta;
+
+coeff0=0.1;
+time=intensity_time(1:11);
+for i=1:height(segment1)
+    mdl1=fitnlm(time, segment1(i,:), modelfun, coeff0);
+    y_hat=modelfun(mdl1.Coefficients, time)
+    figure
+    plot(time, segment1(i,:),...
+        time, y_hat)
+    pause, close
+end
