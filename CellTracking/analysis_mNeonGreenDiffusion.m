ac88ba2350e42b32b0b3d9ee0a3811b7d184df06
za505 <za505@nyu.edu>
2021-12-11 20:55:48 -0500

    determining alpha based on LB 1.2 s, 2 s, and 3 s data

--- CellTracking/analysis_mNeonGreenDiffusion.m
+++ CellTracking/analysis_mNeonGreenDiffusion.m
@@ -13,65 +13,81 @@ clear, close all
 dirsave='/Users/zarina/Documents/MATLAB/MatlabReady/mNeonGreenDiffusion_analysis/12032021_analysis';
 cd([dirsave '/MatFiles'])
 
-datadir1=dir(['10232021_Exp1' '*dm.mat']); %LB, rep1
-datadir2=dir(['10262021_Exp1' '*dm.mat']); %LB, rep2
-
-datadir3=dir(['10232021_Exp2' '*dm.mat']); %PBS, 1 hour, rep 1
-datadir4=dir(['10262021_Exp2' '*dm.mat']); %PBS, 1 hour, rep 2
-
-datadir5=dir(['11202021_Exp1' '*dm.mat']); %LB, frame rate=1.2 s
+datadir1=dir(['11202021_Exp1' '*dm.mat']); %LB, frame rate = 1.2 s
+datadir2=dir(['12082021_Exp1' '*dm.mat']); %LB, frame rate = 2 s
+datadir3=dir(['12082021_Exp2' '*dm.mat']); %LB, frame rate = 3 s
 
 %% calculate corrected fluorescence traces
 %testing whether the correction is better when traces are normalized to
-%initial post-lysis frame
-[normintensity1, Cnew1, tme1, idx1, B1, positions1]=photoCorrect(datadir1, 10, 16.9441);
-[normintensity2, Cnew2, tme2, idx2, B2, positoins2]=photoCorrect(datadir2, 9, 16.9441);
+% %initial post-lysis frame
+[normintensity1, adjintensity1, Cnew1, tme1, tau1, yhat1]=photoCorrect(datadir1, 9, 28.9210);
+[normintensity2, adjintensity2, Cnew2, tme2, tau2, yhat2]=photoCorrect(datadir2, 9, 28.9210);
+[normintensity3, adjintensity3, Cnew3, tme3, tau3, yhat3]=photoCorrect(datadir3, 9, 28.9210);
 
-[normintensity3, Cnew3, tme3, idx3, B3, positions3]=photoCorrect(datadir3, 6, 16.9441);
-[normintensity4, Cnew4, tme4, idx4, B4, positions4]=photoCorrect(datadir4, 9, 16.9441);
+% %% plot normalized traces vs corrected traces
+comparePlot(normintensity1, Cnew1, tme1)
+comparePlot(normintensity2, Cnew2, tme2)
+comparePlot(normintensity3, Cnew3, tme3)
 
-[normintensity5, Cnew5, tme5, idx5, B5, positions5]=photoCorrect(datadir5, 9, 16.9441);
+comparePlot(normintensity1, yhat1, tme1)
+comparePlot(normintensity2, yhat2, tme2)
+comparePlot(normintensity3, yhat3, tme3)
 
-%% plot normalized traces vs corrected traces
-% comparePlot(normintensity1, Cnew1, tme1)
-% comparePlot(normintensity2, Cnew2, tme2)
-% 
-% comparePlot(normintensity3, Cnew3, tme3)
-% comparePlot(normintensity4, Cnew4, tme4)
-% 
-% comparePlot(normintensity5, Cnew5, tme5)
-%% are the cells with increasing traces actually brighter? 
-compareImage(datadir1, 10, idx1, B1, positions1, Cnew1)
+% comparePlot(adjintensity1(1:9, 8:358), adjintensity2(:, 8:358), tme2)
+%% calculate alpha based on tau values
+% find the mean and standard deviation of the time constants
+tau_means = [mean(tau1, 'omitnan'), mean(tau2, 'omitnan'), mean(tau3, 'omitnan')];
+tau_std = [std(tau1, 0, 'omitnan'), std(tau2, 0, 'omitnan'), std(tau3, 0, 'omitnan')];
+
+linearCoef1 = polyfit([repelem(1.2/60, length(tau1)), repelem(2/60, length(tau2)), repelem(3/60, length(tau3))],[tau1, tau2, tau3],1);
+linearFit1= polyval(linearCoef1,[0, repelem(1.2/60, length(tau1)), repelem(2/60, length(tau2)), repelem(3/60, length(tau3))]);
+
+cd(dirsave)
+figure, hold on
+scatter(repelem(1.2/60, length(tau1)), tau1, 'MarkerFaceColor', '#A1D5F7', 'MarkerEdgeColor', '#A1D5F7')
+scatter(repelem(2/60, length(tau2)), tau2, 'MarkerFaceColor', '#A1D5F7', 'MarkerEdgeColor', '#A1D5F7')
+scatter(repelem(3/60, length(tau3)), tau3, 'MarkerFaceColor', '#A1D5F7', 'MarkerEdgeColor', '#A1D5F7')
+
+scatter([1.2/60,2/60, 3/60], tau_means, 'MarkerFaceColor', 'black')
+errorbar(1.2/60, tau_means(1), tau_std(1), 'Color', 'black')
+errorbar(2/60, tau_means(2), tau_std(2), 'Color', 'black')
+errorbar(3/60, tau_means(3), tau_std(3), 'Color', 'black')
+
+plot([0, repelem(1.2/60, length(tau1)), repelem(2/60, length(tau2)), repelem(3/60, length(tau3))], linearFit1, '--b')
+xlim([0, 0.06])
+ylabel('\tau (min^{-1})')
+xticks([0 1.2/60 2/60 3/60])
+xticklabels({'0', '1.2 s', '2 s', '3 s'})
+title('Tau vs Frame Rate')
+% saveas(gcf, 'alpha.fig')
+% saveas(gcf, 'alpha.png')
+
 
 %% Functions
-function [normintensity, Cnew, tme, idx, bounds, positions]=photoCorrect(datadir, imstart, parameter)
+function [normintensity, adjintensity, Cnew, tme, tau, yhat]=photoCorrect(datadir, imstart, parameter)
         
         %pre-allocate variables
         intensity=[];
-        bounds={};
-        positions=nan(1, length(datadir));
-        
+      
         %go through the data for each position
         for i=1:length(datadir)
             
             %load decayMeasure .mat file
             cd(datadir(i).folder)
-            load(datadir(i).name, 'icell_intensity', 'time', 'B')
+            load(datadir(i).name, 'icell_intensity', 'time')
 
             for n=1:height(icell_intensity)
 
                 %make sure there are fluor readings during the initial frame and the final frame, otherwise the adjust. and norm. will look off
                 if ~isnan(icell_intensity(n, imstart))&~isnan(icell_intensity(n, end)) 
                     intensity=[intensity; icell_intensity(n, :)];
-                    bounds=[bounds; {B{n, imstart:end}}];
                 end
             end
 
             if i==1
                 tme=time(imstart:end)-time(imstart); %new time vector
             end
-            
-            positions(i) = height(intensity);
+
         end
 
         %adjust the background
@@ -101,7 +117,7 @@ function [normintensity, Cnew, tme, idx, bounds, positions]=photoCorrect(datadir
             yhat=[yhat; y_hat]; 
 
         end
-        
+    
         %calculate dt (there's variation of dt during initial values, so it's
         %easier to use end values)
         dt=tme(end)-tme(end-1);
