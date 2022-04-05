887ace353ed987a7dc30b0dc084b248f3eb5cc6e
za505 <za505@nyu.edu>
2022-04-05 13:40:11 -0400

    before 04/05/2022 edits

--- CellTracking/PhotoBleachingSim2.m
+++ CellTracking/PhotoBleachingSim2.m
@@ -9,29 +9,36 @@ clear, close all
 % %adjustment is
 % Cunb = Cunb + randn(1, length(Cunb));
 %% Inputs
-check1=0;
-check2=0;
-check3=1;
 
 colorcode={'#0072BD', '#D95319', '#EDB120', '#7E2F8E', '#77AC30', '#4DBEEE', '#A2142F'};
 %% Modeling Photobleach Correction
 %bounds: 0 <= gamma <= 1
 %        0 < dt
 
+%inputs: number of time points, dt, and gamma (permeability coefficient)
+%outputs: 
+% T = time vector
+% Cbl = concentration of bleached fluor.
+% Cunb = concentration of unbleached fluor. or measured fluor.
+% Ctot = total concentration of intracellular fluor.
+% M = modeled fluor concentration using the Newton Method, exp(-t*gamma)
+% beta = photobleaching coeff. calculated as a func of dt
+% F = modeled fluor concentration using a MATLAB function, exp(-t*gamma)
+
 %inputs: N (number of time steps), dt, gamma
 %LB control, frame rate = 1 min
-[T1, Cbl1, Cunb1, Ctot1, M1, beta1] = simRun(100, 1, 0.01);
+[T1, Cbl1, Cunb1, Ctot1, M1, beta1, F1] = simRun(100, 1, 1);
 
 %LB control, frame rate = 30 s
-[T2, Cbl2, Cunb2, Ctot2, M2, beta2] = simRun(100, 0.5, 0.01);
+[T2, Cbl2, Cunb2, Ctot2, M2, beta2, F2] = simRun(100, 0.5, 0.1);
 
 %LB control, frame rate = 15 s
-[T3, Cbl3, Cunb3, Ctot3, M3, beta3] = simRun(100, 0.25, 0.01);
+[T3, Cbl3, Cunb3, Ctot3, M3, beta3, F3] = simRun(100, 0.25, 0.05);
 
 %PBS incubation, frame rate = 1 min
-[T4, Cbl4, Cunb4, Ctot4, M4, beta4] = simRun(100, 1, 0.8);
+[T4, Cbl4, Cunb4, Ctot4, M4, beta4, F4] = simRun(100, 1, 0.03);
 
-%% how much does the 'actual' differ from C total?
+%% how much does the 'actual' differ from C total? how much do the modeled values differ from each other?
 if check1==1
     figure, hold on
     plot(T1, M1, 'Color', colorcode{1}, 'LineStyle', '-') 
@@ -42,6 +49,7 @@ if check1==1
     plot(T3, Ctot3, 'Color', colorcode{3}, 'LineStyle', '--') 
     plot(T4, M4, 'Color', colorcode{4}, 'LineStyle', '-') 
     plot(T4, Ctot4, 'Color', colorcode{4}, 'LineStyle', '--') 
+    title('how much does the actual (M) differ from C total?')
 % legend({'LB, frame rate = 1 min, actual', 'LB, frame rate = 1 min, measured', 'LB, frame rate = 30 s, actual', 'LB, frame rate = 30 s, measured', 'LB, frame rate = 15 s, actual', 'LB, frame rate = 15 s, measured', 'PBS, frame rate = 1 min, actual', 'PBS, frame rate = 1 min, measured'})
 end
 % Observation: they do not differ, which is what we expect
@@ -65,10 +73,10 @@ end
 %unnecessary
 
 %% Can we obtain the 'actual' fluorescence value given only the measured values?
-[Cnew1, dCB1, dCU1, dCP1, tau1, gamma1] = simReverse(Cunb1, T1, 1, beta1);
-[Cnew2, dCB2, dCU2, dCP2, tau2, gamma2] = simReverse(Cunb2, T2, 0.5, beta2);
-[Cnew3, dCB3, dCU3, dCP3, tau3, gamma3] = simReverse(Cunb3, T3, 0.25, beta3);
-[Cnew4, dCB4, dCU4, dCP4, tau4, gamma4] = simReverse(Cunb4, T4, 1, beta4);
+[Cnew1, dCB1, dCU1, dCP1, tau1, gamma1] = simReverse(Cunb1, T1, 1);
+[Cnew2, dCB2, dCU2, dCP2, tau2, gamma2] = simReverse(Cunb2, T2, 0.5);
+[Cnew3, dCB3, dCU3, dCP3, tau3, gamma3] = simReverse(Cunb3, T3, 0.25);
+[Cnew4, dCB4, dCU4, dCP4, tau4, gamma4] = simReverse(Cunb4, T4, 1);
 
 %% how close are the photocorrected values and the 'actual' fluor?
 if check3==1
@@ -82,15 +90,31 @@ if check3==1
     plot(T4, M4, 'Color', colorcode{4}, 'LineStyle', 'none', 'Marker', 'o') 
     plot(T4, Cnew4, 'Color', colorcode{4}, 'LineStyle', '-') 
 end
+
+%% Does F = exp(time*-gamma) equal M?
+if check4==1
+    figure, hold on
+    plot(T1, M1, 'Color', colorcode{1}, 'LineStyle', 'none', 'Marker', 'o') 
+    plot(T1, F1, 'Color', colorcode{1}, 'LineStyle', '-') 
+    plot(T2, M2, 'Color', colorcode{2}, 'LineStyle', 'none', 'Marker', 'o') 
+    plot(T2, F2, 'Color', colorcode{2}, 'LineStyle', '-') 
+    plot(T3, M3, 'Color', colorcode{3}, 'LineStyle', 'none', 'Marker', 'o') 
+    plot(T3, F3, 'Color', colorcode{3}, 'LineStyle', '-') 
+    plot(T4, M4, 'Color', colorcode{4}, 'LineStyle', 'none', 'Marker', 'o') 
+    plot(T4, F4, 'Color', colorcode{4}, 'LineStyle', '-') 
+    title('how much do the modeled values differ from each other?')
+end
+
 %% Functions
-function [T, Cbl, Cunb, Ctot, M, beta] = simRun(N, dt, gamma)
+function [T, Cbl, Cunb, Ctot, M, beta, F] = simRun(N, dt, gamma)
 
 % N = number of minutes
 % dt = increment in minutes, frame rate 
 % gamma = permeability coefficient
 
 T = [0:dt:N]; %time vector in minutes
-tau = (16.0806)*dt + 0.4234;
+%tau = (16.0806)*dt + 0.4234;
+tau = (30)*dt;
 beta = 1/tau;
 
 %I hypothesize that measured fluorescence decreases according to the following
@@ -102,9 +126,9 @@ beta = 1/tau;
 %pre-allocte 'measured' fluorescence variables
 %assume that our initial measured fluorescence is equal to the initial 'no diffusion' fluorescence
 
-Cbl=zeros(size(T));%Normalized concentration of bleached fluorophores
-Cunb=zeros(size(T));%Normalized concentration of unbleached fluorophores
-Ctot=zeros(size(T));%Normalized concentration of total fluorophores
+Cbl=nan(size(T));%Normalized concentration of bleached fluorophores
+Cunb=nan(size(T));%Normalized concentration of unbleached fluorophores
+Ctot=nan(size(T));%Normalized concentration of total fluorophores
 Cunb(1)=1;%Initial concentration of unbleached fluorophores is 1
 Cbl(1)=0;%Initial concentration of unbleached fluorophores is 0
 Ctot(1)=1;
@@ -139,9 +163,14 @@ for i=1:length(T)-1
     M(i+1) = M(i) + dM;
 end
 
+%can I get the same fluor. values as M using the equation C =
+%exp(time.*-gamma)?
+modelExp=@(gamma,time)exp(time.*-gamma);
+F = modelExp(gamma, T);
+
 end
 
-function [Cnew, dCB, dCU, dCP, tau, gamma] = simReverse(Cunb, T, dt, beta)
+function [Cnew, dCB, dCU, dCP, tau, gamma] = simReverse(Cunb, T, dt)
 
     %first, let's fit the measured fluor. values to an exponential to calculate
     %tau
@@ -153,7 +182,9 @@ function [Cnew, dCB, dCU, dCP, tau, gamma] = simReverse(Cunb, T, dt, beta)
 
 
 %can I work in the reverse direction and get M using alpha (the slope of tau vs frame rate)?
-alpha=1/beta/dt; %the proportion of photobleaching
+%alpha=1/beta/dt; %the proportion of photobleaching
+beta = 1/((16.0806)*dt + 0.4234);
+alpha=1/beta/dt;
 
 %assume that the initial 'measured' fluorescence values and corrected
 %fluor. values will be equal
