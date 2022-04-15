26cb8c12dfc973a6ae0fdf78c069ea714485f24b
za505 <za505@nyu.edu>
2022-04-15 14:48:30 -0400

    code for dm data aggregation

--- CellTracking/sanity_checks.m
+++ CellTracking/sanity_checks.m
@@ -6,104 +6,19 @@
 
 clear, close all
 
-%Is there a difference in the background/autofluorescence value of cells
-%perfused with LB vs LB + LB + IPTG? Let's look at cases where the frame
-%rate is 1 minute from beginning to end.
-
-%% initial perfusion with LB + IPTG, final perfusion with LB
-dirpath = '/Users/zarina/Downloads/NYU/Year3_2022_Spring/mNeonGreen_analysis/rawFiles/';
-basename = '04042022_Exp1';
+%% the goal is to aggregate the 20% intensity data
+dirpath = '/Users/zarina/Downloads/NYU/Year3_2022_Spring/mNeonGreen_analysis/raw/';
+dirsave = '/Users/zarina/Downloads/NYU/Year3_2022_Spring/mNeonGreen_analysis/aggregate/';
+basename = '10262021_Exp1';
 
 cd(dirpath)
     
 datadir = dir([basename '*']);
 [intensity, bgintensity, lcell, time] = dataAggregate(datadir);
 
-%in a plot of background intensity vs time, it doesn't appear as though the
-%rate of photobleaching changes. But this is just by eye. There must be a
-%more rigorous approach.
-figure
-plot(time, bgintensity)
+cd(dirsave)
+save(basename)
 
-%ignore time points when detergent is perfused
-time1 = time;
-time1(5:9) = NaN;
-
-bgTrace1 = bgintensity;
-bgTrace1(:, 5:9) = NaN;
-
-%% initial perfusion with LB + IPTG, final perfusion with LB + IPTG. Includes the CY5 channel.
-dirpath = '/Users/zarina/Downloads/NYU/Year3_2022_Spring/mNeonGreen_analysis/rawFiles/';
-basename = '10232021_Exp1';
-
-cd(dirpath)
-    
-datadir = dir([basename '*']);
-[intensity, bgintensity, lcell, time] = dataAggregate(datadir);
-
-%in a plot of background intensity vs time, it doesn't appear as though the
-%rate of photobleaching changes. But this is just by eye. There must be a
-%more rigorous approach.
-figure
-plot(time, bgintensity)
-
-%ignore time points when detergent is perfused
-time2 = time;
-time2(5:9) = NaN;
-
-bgTrace2 = bgintensity;
-bgTrace2(:, 5:9) = NaN;
-
-%% initial perfusion with LB + IPTG, final perfusion with LB + IPTG. Includes the CY5 channel.
-dirpath = '/Users/zarina/Downloads/NYU/Year3_2022_Spring/mNeonGreen_analysis/rawFiles/';
-basename = '10262021_Exp1'
-
-cd(dirpath)
-    
-datadir = dir([basename '*']);
-[intensity, bgintensity, lcell, time] = dataAggregate(datadir);
-
-%in a plot of background intensity vs time, it doesn't appear as though the
-%rate of photobleaching changes. But this is just by eye. There must be a
-%more rigorous approach.
-figure
-plot(time, bgintensity)
-
-%ignore time points when detergent is perfused
-time3 = time;
-time3(5:9) = NaN;
-
-bgTrace3 = bgintensity;
-bgTrace3(:, 5:9) = NaN;
-
-%% initial perfusion with LB + IPTG, final perfusion with LB. 20% intensity
-dirpath = '/Users/zarina/Downloads/NYU/Year3_2022_Spring/mNeonGreen_analysis/rawFiles/';
-basename = '02212022_Exp2';
-
-cd(dirpath)
-    
-datadir = dir([basename '*']);
-[intensity, bgintensity, lcell, time] = dataAggregate(datadir);
-
-%in a plot of background intensity vs time, it doesn't appear as though the
-%rate of photobleaching changes. But this is just by eye. There must be a
-%more rigorous approach.
-figure
-plot(time, bgintensity)
-
-%ignore time points when detergent is perfused
-time4 = time;
-time4(5:9) = NaN;
-
-bgTrace4 = bgintensity;
-bgTrace4(:, 5:9) = NaN;
-
-%% plot to compare the three
-figure, hold on
-plot(time1, bgTrace1, '-r')
-plot(time2, bgTrace2, '-b')
-plot(time3, bgTrace3, '-g')
-plot(time4, bgTrace4, '-k')
 
 %% Functions
 %to aggregate data from a given experiment
